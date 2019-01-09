6fbb17f8b0b7ad9f221283ff6e2d8c1934c7ddc4
Seth <abramsseth@gmail.com>
2019-01-09 15:46:13 -0500

    first commit - search begin

---
+++ src/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchBar from 'material-ui-search-bar';
+import Script from 'react-load-script';
+
+class Search extends Component{
+    constructor(props){
+        super(props);
+        
+        this.state= {
+            city: '',
+            query: ''
+        };    
+   
+    }
+
+    
+}
